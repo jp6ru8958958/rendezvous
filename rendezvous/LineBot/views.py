6b05643c244cce154b5ab885dd9c93046aae6e43
jp6ru8958958 <jp6ru8958958@gmail.com>
2021-06-10 11:36:24 +0800

    timezone bug fix

--- rendezvous/LineBot/views.py
+++ rendezvous/LineBot/views.py
@@ -10,6 +10,7 @@ from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import *
 
 from datetime import datetime
+import pytz
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
